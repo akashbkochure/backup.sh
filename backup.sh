255a6dbea6582b319749a2bf46ece7c1baaa0a99
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-09-30 15:01:30 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the source and destination paths
+SOURCE_PATH="/var/www/html"  # Adjust to your website's path
+BACKUP_PATH="/home/ubuntu/akash/backups"  # Adjust to your desired backup location
+
+# Create a timestamp for the backup file
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="backup_$TIMESTAMP.tar.gz"
+
+# Create the backup using tar
+tar -czf "$BACKUP_PATH/$BACKUP_FILE" -C "$SOURCE_PATH" .
+
+echo "Backup completed at $TIMESTAMP"
